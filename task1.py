506a6a975861a4816f53b5c191e62b8d7c0af402
lluidesia <liudaprysiazhna@gmail.com>
2016-10-07 22:54:38 +0300

    Task 1 improve v6

--- task1.py
+++ task1.py
@@ -1,23 +1,22 @@
-print("*"*47)
-print("*"*3+" "*15+"Greetings!"+" "*16+"*"*3)
-print("*"*3+" "*4+"Just a test app that prints title"+" "*4+"*"*3)
-print("*"*47)
+print("**********************************")
+print("*********** Greetings!************")
+print("********* This is task 1 *********")
+print("**********************************")
 print()
 print("What is the average sleeping time that you slept during last year?")
 
 sleep_average = float(input())
 
-while(sleep_average<=0):
-	sleep_average = float(input("This is strange, try again: ", ))
+while(sleep_average <= 0):
+    sleep_average = float(input("This is strange, try again: ", ))
 
-sleep_difference=(sleep_average-8)*365/24
+sleep_dif = (sleep_average - 8) * 365 / 24
+print("Your differens with normal sleep time is {:.1f} days a year.".format(
+    sleep_dif))
 
-if sleep_difference==0:
-	print("Your sleep differens with normal sleep time is %.1f days a year." %sleep_difference)
-	print("You are normal human, live long and prosper!")
-elif sleep_difference<0:
-	print("Your sleep differens with normal sleep time is %.1f days a year." %sleep_difference)
-	print("What a hell are you doing? Don't you want to live long and prosper?")
-elif sleep_difference>0:
-	print("Your sleep differens with normal sleep time is %.1f days a year." %sleep_difference)
-	print("You like to sleep Lazy Ass, aren't you?")
+if sleep_dif == 0:
+    print("You are normal human, live long and prosper!")
+elif sleep_dif < 0:
+    print("What a hell are you doing?Don't you want to live long and prosper?")
+elif sleep_dif > 0:
+    print("You like to sleep Lazy Ass, aren't you?")
