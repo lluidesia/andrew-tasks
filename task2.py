c7a10d10c1c569ffb1f97e6fb9b0ddd84e108878
lluidesia <liudaprysiazhna@gmail.com>
2016-11-06 00:45:05 +0200

    Task 2 Part 2

--- task2.py
+++ task2.py
@@ -1,8 +1,6 @@
+import sqlite3
 import random
 import time
-
-print("Let's play!")
-time_start = time.time()
 
 
 def quiz():
@@ -22,12 +20,45 @@ def quiz():
         answer_user = int(input(text_wrong,))
 
 
-for i in range(10):
-    quiz()
+def game():
+    print("Let's play!")
+    time_start = time.time()
 
-time_finish = time.time()
-time_spent = int(time_finish - time_start)
-mins = int(time_spent / 60)
-secs = time_spent % 60
-result = "You spent {} minutes and {} seconds".format(mins, secs)
-print(result)
+    for i in range(10):
+        quiz()
+
+    time_finish = time.time()
+    time_spent = int(time_finish - time_start)
+    mins = int(time_spent / 60)
+    secs = time_spent % 60
+    result = "{} minutes and {} seconds".format(mins, secs)
+    print("You spent " + result)
+    player_name = input("Enter your name: ", )
+
+    c.execute("INSERT INTO People VALUES (?, ?)", (player_name, result))
+    conn.commit()
+
+
+def show_results():
+    c.execute('SELECT * from People')
+    if len(c.fetchall()) <= 0:
+        print("No results yet")
+    for row in c.execute('SELECT * from People'):
+        print(row)
+
+
+conn = sqlite3.connect('leaderboard.db')
+c = conn.cursor()
+c.execute("CREATE TABLE if not exists People (Name, Result)")
+
+menu_item = 0
+
+while menu_item != 3:
+    print("\nHi! This is menu:\n\t1. Start game\n\t2. See results\n\t3. Quit")
+    menu_item = int(input("What would you like to do? ", ))
+
+    if menu_item == 1:
+        game()
+
+    elif menu_item == 2:
+        show_results()
